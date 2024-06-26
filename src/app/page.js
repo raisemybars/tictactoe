05fe0617e05caa4d080c47ce41d844a4f22120af
Brandon <raisemybars@gmail.com>
2024-06-26 15:01:18 -0700

    add header

--- src/app/page.js
+++ src/app/page.js
@@ -99,6 +99,7 @@ export default function Game() {
 
   return (
     <div className="game">
+      <h1>Testing auto deloy</h1>
       <div className="game-board">
         <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} />
       </div>
